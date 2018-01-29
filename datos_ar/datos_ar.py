263313702765b15cce6649e484e59b8669c8a751
Juan Montalvo Bressi <juan-m12i@users.noreply.github.com>
2018-01-29 14:07:03 +0000

    Update datos_ar.py

--- datos_ar/datos_ar.py
+++ datos_ar/datos_ar.py
@@ -20,9 +20,43 @@ url = "http://apis.datos.gob.ar/series/api/series?"
 
 COLLAPSE_VALUES = ["year", "quarter", "month", "week", "day"]
 COLLAPSE_AGGREGATION_VALUES = ["avg", "sum", "end_of_period", "min", "max"]
+REPRESENTATION_VALUES = ["value", "change", "percent_change", "percent_change_a_year_ago"]
+FORMAT_VALUES = ["json", "csv"]
+HEADER_VALUES = ["titles", "ids", "descriptions"]
+SORT_VALUES = ["asc", "desc"]
+METADATA_VALUES = ["none", "simple", "full", "only"]
+
+
+def get_datos(ids, format = "json", start_date = None, end_date = None, collapse = None,
+              collapse_aggregation = None, representation = "value", limit = 100, start = 0,
+              header = "titles", sort = "asc", 
+              
+              
 
 
 
-def get_datos(series, format = "json", start_date = None, end_date = None, collapse = None, collapse_aggregation = None
+"""              
+              
+              Nombre	Requerido	Tipo	Default	Ejemplos
 
+collapse	No	Uno de: day, week, month, quarter, year	La frecuencia propia de la serie	collapse=year
+collapse=quarter
+collapse_aggregation	No	Uno de: avg, sum, end_of_period, min, max	avg	collapse_aggregation=sum
+limit	No	Número entero positivo, no mayor que 1000.	100	limit=50
+start	No	Número entero positivo o 0.	0	start=100
+start_date	No	Fecha y hora en formato ISO 8601.
 
+Si no se especifica este parámetro, se devuelven los datos disponibles para la serie o series desde el valor más antiguo.	N/A	start_date=2016-11-30
+start_date=2016-11
+start_date=2016
+end_date	No	Fecha y hora en formato ISO 8601.
+
+Si no se especifica este parámetro, se devuelven los datos disponibles para la serie o series hasta el valor más reciente.	N/A	end_date=2016-11-30
+end_date=2016-11
+end_date=2016
+format	No	Uno de: json, csv	json	format=csv
+header	No	Uno de: titles, ids, descriptions	titles	header=ids
+sort	No	Uno de: asc, desc	asc	sort=desc
+metadata	No	Uno de: none, simple, full, only	simple	metadata=none
+
+"""
